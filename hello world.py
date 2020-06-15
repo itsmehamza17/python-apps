d0d22e5c30bd8ddb5e8a1d3e0f25f62b2cd9e81b
itsmehamza17 <itsmehamza17@gmail.com>
2020-06-15 01:32:54 +0100

    hello world and multiline new line

--- hello world.py
+++ hello world.py
@@ -1,2 +1,6 @@
-print('hello world')
-print('1')
+#this is hello world application
+print('hello world \n')
+print('again, hello world')
+#multiline
+print('''this is multiline hello world
+this day is a special day''')
