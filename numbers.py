b42df00b99d99fd5aeba51dff80b82c06d000aa1
itsmehamza17 <itsmehamza17@gmail.com>
2020-06-16 16:48:42 +0100

    numbers (added numbers code)

---
+++ numbers.py
@@ -0,0 +1,17 @@
+print(123 +  int('132'))
+print(45 / 9)
+print(3 * 5)
+print(15.5 - 15)
+print(2 ** 10)
+
+num1 = 50.5
+num2 = 30
+
+total = (num1 + num2)
+print(type(total))
+
+
+a1 = 22
+a1 +=3
+a1 = a1 +1
+print(a1)
