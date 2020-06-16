861f211d1278965366bd3876c519ff4252c139fe
itsmehamza17 <itsmehamza17@gmail.com>
2020-06-16 16:13:56 +0100

    strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+fname = 'hamza'
+lname = 'SA'
+print(fname.upper() + ' ' + lname.lower())
+full_name = 'hamza sa'
+print(full_name.replace('hamza','mohamed'))
+B = 'nothing \n' * 20
+print(B)
+value = 9.5
+print(value)
+tr_or_fa = True
+print(tr_or_fa)
