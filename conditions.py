822d210461679eff722c99b2e096a452512b61d6
itsmehamza17 <itsmehamza17@gmail.com>
2020-06-19 15:19:55 +0100

    conditions

---
+++ conditions.py
@@ -0,0 +1,6 @@
+username = input('Please insert your username: ')
+password = input('Please insert your password: ')
+if username == 'hamza1' and password == '12345':
+    print('Welcome to your account')
+else:
+    print('Error: invalid username or password')    
